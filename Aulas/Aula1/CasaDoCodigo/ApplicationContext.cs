9919f584150e6c5361508d0ce5785d8f251f4c1b
Willian Soares <dasilva.williansoares@gmail.com>
2020-10-30 09:56:35 -0300

    Relacionamento e migrações.

--- Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -18,6 +18,19 @@ namespace CasaDoCodigo
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Produto>().HasKey(t => t.Id); //representa a chave primaria da entidade
+            
+            modelBuilder.Entity<Pedido>().HasKey(t => t.Id); 
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Itens).WithOne(t => t.Pedido); 
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Cadastro).WithOne(t => t.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(t => t.Id); 
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Produto);
+
+
+            modelBuilder.Entity<Cadastro>().HasKey(t => t.Id); 
+            modelBuilder.Entity<Cadastro>().HasOne(t => t.Pedido);
+
         }
     }
 }
